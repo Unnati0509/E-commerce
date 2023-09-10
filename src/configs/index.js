b22da7e301aae9f0a8f35500061955e905566437
Unnati <unnatisaxena1605@gmail.com>
2023-09-10 13:29:23 +0530

    resolved issues

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const nodeEnv = process.env.NODE_ENV || 'production';
+const nodeEnv = process.env.NODE_ENV || 'development';
 const port = parseInt(process.env.PORT, 10) || 8080 ;
 const secret = process.env.SESSION_SECRET;
 
