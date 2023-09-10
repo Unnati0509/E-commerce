7203ffb0e045d6d10c7983afb016ff7419cb926b
Unnati <unnatisaxena1605@gmail.com>
2023-09-10 13:17:46 +0530

    addedchanges

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const nodeEnv = process.env.NODE_ENV || 'production';
-const port = parseInt(process.env.PORT, 10) ;
+const port = parseInt(process.env.PORT, 10) || 8080 ;
 const secret = process.env.SESSION_SECRET;
 
 module.exports = {
