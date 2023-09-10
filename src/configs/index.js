b4f4e132f5f8fb458b2399a7558d447bd3f32259
Unnati <unnatisaxena1605@gmail.com>
2023-09-10 12:50:32 +0530

    deploying

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-const port = parseInt(process.env.PORT, 10) || 8080;
+const nodeEnv = process.env.NODE_ENV || 'production';
+const port = parseInt(process.env.PORT, 10) ;
 const secret = process.env.SESSION_SECRET;
 
 module.exports = {
